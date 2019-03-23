6e29d6ad4800ca9d6a1eae99cb5f8c707d9d816e
Hashini <hashinips96@gmail.com>
2019-03-23 09:44:44 +0530

    Add Event connected to the Firebase

--- js/dbFunction.js
+++ js/dbFunction.js
@@ -1,10 +1,21 @@
 // Function to retrieve data from the forms start
-var addEvent = function() {
+var config = {
+    apiKey: "AIzaSyBElcSBIqe2koIyB6pD9aAFglAq82Fu90c",
+    authDomain: "hackaholics-memorymate.firebaseapp.com",
+    databaseURL: "https://hackaholics-memorymate.firebaseio.com",
+    projectId: "hackaholics-memorymate",
+    storageBucket: "hackaholics-memorymate.appspot.com",
+    messagingSenderId: "618931079152"
+  };
+  var app = firebase.initializeApp(config);
+  var db = firebase.firestore(app);
+
+function addEvent() {
     var event_name = document.getElementById("event_name");
     var description = document.getElementById("description");
     var date = document.getElementById("date");
     var time = document.getElementById("time");
-    var privacy = document.getElementsByName("privacy");
+  //  var privacy = document.getElementsByName("privacy");
     var schedule = document.getElementsByName("schedule");
 
     for (var i = 0, length = schedule.length; i < length; i++)
@@ -18,48 +29,243 @@ var addEvent = function() {
             break;
         }
     }
-    for (var i = 0, length = privacy.length; i < length; i++)
-    {
-        if (privacy[i].checked)
-        {
-            // store the checked value in a variable
-            var privacy_policy = privacy[i].value;
+    // for (var i = 0, length = privacy.length; i < length; i++)
+    // {
+    //     if (privacy[i].checked)
+    //     {
+    //         // store the checked value in a variable
+    //         var privacy_policy = privacy[i].value;
 
-            // one radio can be logically checked here
-            break;
-        }
-    }
-    addEventToDatabase(event_name.value, description.value, date.value, time.value, selected_schedule, privacy_policy);
+    //         // one radio can be logically checked here
+    //         break;
+    //     }
+    // }
+     addEventToDatabase(event_name.value, description.value, date.value, time.value, selected_schedule);
+     //privacy_policy
 }
 // Function to retrieve data from the forms end
 
 // Function to pass data to the database start
-function addEventToDatabase(event_name, description, date, time, privacy, schedule) {
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            firebase.database().ref('events/' + user.uid + 'event/').set({
-                eventName: event_name,
-                description: description,
-                date: date,
-                time: time,
-                privacyType: privacy,
-                schedule: schedule
-            });
-        }
-        else {
-                alert("User Not Found");
-        }
-    });
+function addEventToDatabase(event_name, description, date, time, schedule) {
+    db.collection("events").add({
+    name: event_name,
+    description: description,
+    date: date,
+    time: time,
+    privacy:"public",
+    schedule: schedule
+  })
+  .then(function(docRef){
+    console.log("Success");
+    window.location.replace("./app.html");
+  })
+  .catch(function(err){
+    console.error("Failed!");
+  });
 }
 // Function to pass data to the database end
 
-function getEvents() {
-    alert("dsasd");
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            firebase.database().ref('events/' + user.uid).once('value', function(snapshot) {
-                console.log(snapshot.val());
-            });
-        }
+function getEventList(){
+    // db.collection("events").get().then(function(querySnapshot) {
+    //     querySnapshot.forEach(function(doc) {
+    //         // doc.data() is never undefined for query doc snapshots
+    //         console.log(doc.id, " => ", doc.data());
+    //     });
+    // });
+    var temp = 1;
+    db.collection("events").where("date", "==", "23-03-2019")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+            var current_name = 'name'+temp
+            var current_description = 'desc'+temp
+            var current_time = 'time'+temp
+            temp+=1;
+
+            document.getElementById(current_name).innerHTML = doc.data().name;
+            document.getElementById(current_description).innerHTML = doc.data().description;
+            document.getElementById(current_time).innerHTML = doc.data().time;
+
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
     });
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Function to retrieve data from the forms start
+// var addEvent = function() {
+//     var event_name = document.getElementById("event_name");
+//     var description = document.getElementById("description");
+//     var date = document.getElementById("date");
+//     var time = document.getElementById("time");
+//     var privacy = document.getElementsByName("privacy");
+//     var schedule = document.getElementsByName("schedule");
+
+//     for (var i = 0, length = schedule.length; i < length; i++)
+//     {
+//         if (schedule[i].checked)
+//         {
+//             // store the checked value in a variable
+//             var selected_schedule = schedule[i].value;
+
+//             // one radio can be logically checked here
+//             break;
+//         }
+//     }
+//     for (var i = 0, length = privacy.length; i < length; i++)
+//     {
+//         if (privacy[i].checked)
+//         {
+//             // store the checked value in a variable
+//             var privacy_policy = privacy[i].value;
+
+//             // one radio can be logically checked here
+//             break;
+//         }
+//     }
+//     addEventToDatabase(event_name.value, description.value, date.value, time.value, selected_schedule, privacy_policy);
+// }
+// // Function to retrieve data from the forms end
+
+// // Function to pass data to the database start
+// function addEventToDatabase(event_name, description, date, time, privacy, schedule) {
+//     firebase.auth().onAuthStateChanged((user) => {
+//         if (user) {
+//             firebase.database().ref('events/' + user.uid + 'event/').set({
+//                 eventName: event_name,
+//                 description: description,
+//                 date: date,
+//                 time: time,
+//                 privacyType: privacy,
+//                 schedule: schedule
+//             });
+//         }
+//         else {
+//                 alert("User Not Found");
+//         }
+//     });
+// }
+// // Function to pass data to the database end
+
+// function getEvents() {
+//     alert("dsasd");
+//     firebase.auth().onAuthStateChanged((user) => {
+//         if (user) {
+//             firebase.database().ref('events/' + user.uid).once('value', function(snapshot) {
+//                 console.log(snapshot.val());
+//             });
+//         }
+//     });
+// }
